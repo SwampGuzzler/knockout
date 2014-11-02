f40e59cf0e6c9bbf69feee1d95df10c02c6b1bc8
Lucas Cotner <lc07@uw.edu>
2014-11-02 10:35:07 -0500

    Integrated Knockout, esri leaflet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 //var name = ko.observable("Lucas");
-var nameVar = {name: ko.observable("Lucas")};
-ko.applyBindings(nameVar);
+var MyViewModel = function() {
+	this.name = ko.observable("Lucas");
+	this.parkCount = ko.computed(function() {
+		return 5
+	});
+
+};
+
+
+var map = L.map('map').setView([45.528, -122.680], 13);
+
+L.esri.basemapLayer("Gray").addTo(map);
+
+var parks = new L.esri.FeatureLayer("http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Portland_Parks/FeatureServer/0", {
+	style: function () {
+	  return { color: "#70ca49", weight: 2 };
+	}
+}).addTo(map);
+
+var popupTemplate = "<h3>{NAME}</h3>{ACRES} Acres<br><small>Property ID: {PROPERTYID}<small>";
+
+var counter = 0;
+parks.bindPopup(function(feature){
+	counter++;
+	return L.Util.template(popupTemplate, feature.properties)
+});
+
+
+ko.applyBindings(new MyViewModel);

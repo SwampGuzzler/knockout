b5eedb77f553c2028040e5cc43b79c534318c3c6
Lucas Cotner <lc07@uw.edu>
2014-11-03 21:47:01 -0500

    Working spatial queries

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-//var name = ko.observable("Lucas");
+
 var southWest = L.latLng(45.518, -122.708);
 var northEast = L.latLng(45.537, -122.651);
 var bounds = L.latLngBounds(southWest, northEast);
 var query = L.esri.Tasks.query('http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Portland_Parks/FeatureServer/0');
 query.within(bounds);
-//console.log(L.getBounds());
+
 var parksCurrent = 3;
 
 query.run(function(error, featureCollection, response){
 	parksCurrent = featureCollection.features.length;
 	MyViewModel.computeParks(parksCurrent);
-    console.log('Found ' + featureCollection.features.length + ' features');
 });
 
 
@@ -31,8 +30,7 @@ var parks = new L.esri.FeatureLayer("http://services.arcgis.com/rOo16HdIMeOBI4Mb
 }).addTo(map);
 var popupTemplate = "<h3>{NAME}</h3>{ACRES} Acres<br><small>Property ID: {PROPERTYID}<small>";
 console.log(map);
-console.log(map.getBounds());
-//console.log(map._onResize);
+
 map.on("dragend", function(e) {
     updateParks();
 });
@@ -40,35 +38,24 @@ map.on("zoomend", function(e) {
     updateParks();
 });
 var updateParks = function() {
-	var bound = map.getBounds();
-
-	var sW = bound._southWest;
-	var nE = bound._northEast;
-	console.log(sW);
+	var bounding = map.getBounds();
+	var sW = bounding._southWest;
+	var nE = bounding._northEast;
 	var viewBounds = L.latLngBounds(sW, nE);
-	debugger;
 	var spatialQuery = L.esri.Tasks.query('http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Portland_Parks/FeatureServer/0');
-	spatialQuery.within(bounds);
+	spatialQuery.within(viewBounds);
 
 	spatialQuery.run(function(error, featureCollection, response){
 		parksCurrent = featureCollection.features.length;
 		MyViewModel.computeParks(parksCurrent);
-	    console.log('Found ' + featureCollection.features.length + ' features');
 	});
 }
-
-
-
-//console.log(parks);
 
 parks.bindPopup(function(feature){
 	return L.Util.template(popupTemplate, feature.properties)
 });
 
+ko.applyBindings(MyViewModel);
 
-//setTimeout(function() {
-	ko.applyBindings(MyViewModel);
-
-//}, 5000);
 
 
